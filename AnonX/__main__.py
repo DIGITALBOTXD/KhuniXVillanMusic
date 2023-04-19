c886d42d98b588b375ddcbbe8b90ac87ac95903b
DIGITAL BOT XD <128338349+DIGITALBOTXD@users.noreply.github.com>
2023-04-19 10:04:36 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -53,7 +53,7 @@ async def init():
     await Anon.start()
     try:
         await Anon.stream_call(
-            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
+            "https://graph.org/file/7bd7950c4d7a77b8e4b96.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AnonX").error(
